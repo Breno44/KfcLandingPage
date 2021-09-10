cec6d3e0bd14aa506aa1a13495b8bf69316353e2
Breno Henrique <bhfs.2908@gmail.com>
2021-09-10 18:21:32 -0300

    feat:finish

--- src/App.elements.js
+++ src/App.elements.js
@@ -8,6 +8,10 @@ export const Main = styled.section`
   justify-content: center;
   align-items: center;
   padding: 30px 100px;
+
+  @media (max-width: 991px) {
+    padding: 40px;
+  }
 `;
 
 export const Header = styled.header`
@@ -20,6 +24,10 @@ export const Header = styled.header`
   justify-content: space-between;
   align-items: center;
   z-index: 1000;
+
+  @media (max-width: 991px) {
+    padding: 20px 40px;
+  }
 `;
 
 export const Ai = styled.a``;
@@ -33,10 +41,18 @@ export const Ali = styled.a`
 
 export const Logo = styled.img`
   max-width: 90px;
+
+  @media (max-width: 991px) {
+    max-width: 70px;
+  }
 `;
 
 export const Navigation = styled.ul`
   display: flex;
+
+  @media (max-width: 991px) {
+    display: none;
+  }
 `;
 
 export const Li = styled.li`
@@ -49,17 +65,30 @@ export const Content = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width: 991px) {
+    flex-direction: column;
+    margin: 120px 0 40px;
+  }
 `;
 
 export const Text = styled.div`
   width: 100%;
   max-width: 600px;
+
+  @media (max-width: 991px) {
+    max-width: 100%;
+  }
 `;
 
 export const H2 = styled.h2`
   color: #fff;
   font-size: 5em;
   font-weight: 300;
+
+  @media (max-width: 991px) {
+    font-size: 2em;
+  }
 `;
 
 export const Span = styled.span`
@@ -93,6 +122,10 @@ export const OrderNow = styled.a`
 
 export const Slider = styled.div`
   position: relative;
+
+  @media (max-width: 991px) {
+    margin-top: 40px;
+  }
 `;
 
 export const Slides = styled.div`
@@ -101,5 +134,121 @@ export const Slides = styled.div`
   img {
     width: 100%;
     max-width: 600px;
+
+    @media (max-width: 991px) {
+      width: 420px;
+    }
+
+    @media (max-width: 760px) {
+      width: 220px;
+    }
+  }
+`;
+
+export const Footer = styled.footer`
+  position: absolute;
+  width: 100%;
+  display: flex;
+  padding: 30px 100px;
+  justify-content: space-between;
+  align-items: flex-end;
+  bottom: 0;
+
+  @media (max-width: 760px) {
+    padding: 5px 10px;
+  }
+`;
+
+export const Sci = styled.ul`
+  display: flex;
+
+  li {
+    list-style: none;
+
+    a {
+      color: #fff;
+      font-size: 2em;
+      margin-right: 20px;
+      display: inline-block;
+      transition: 0.25s;
+
+      &:hover {
+        transform: translateY(-5px);
+      }
+    }
+  }
+`;
+
+export const PrevNext = styled.div`
+  position: relative;
+  user-select: none;
+
+  p {
+    position: relative;
+    color: #fff;
+    text-align: end;
+    margin-bottom: 15px;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: -35px;
+      transform: translateY(-50%);
+      width: 50px;
+      height: 2px;
+      background: #fff;
+    }
+  }
+`;
+
+const SpanDefault = styled.span`
+  position: relative;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  border: 2px solid #fff;
+  cursor: pointer;
+  font-size: 2em;
+  color: #fff;
+`;
+
+export const Prev = styled(SpanDefault)`
+  margin-right: 20px;
+`;
+
+export const Next = styled(SpanDefault)``;
+
+export const Toggle = styled.div`
+  @media (max-width: 991px) {
+    position: relative;
+    width: 32px;
+    height: 40px;
+    background: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999999;
+    cursor: pointer;
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      background: #fff;
+      transform: translateY(-10px);
+      box-shadow: 0 10px 0 #fff;
+    }
+    &::after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      background: #fff;
+      transform: translateY(10px);
+    }
   }
 `;
